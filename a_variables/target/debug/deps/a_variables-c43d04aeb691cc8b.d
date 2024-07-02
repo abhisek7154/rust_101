83eea66a53b9a78c4aed31b948038c3c27180fbf
unknown <abhiperl1000@gmail.com>
2024-07-02 18:01:40 +0530

    test

---
+++ a_variables/target/debug/deps/a_variables-c43d04aeb691cc8b.d
@@ -0,0 +1,5 @@
+D:\rust_101\a_variables\target\debug\deps\liba_variables-c43d04aeb691cc8b.rmeta: src/main.rs
+
+D:\rust_101\a_variables\target\debug\deps\a_variables-c43d04aeb691cc8b.d: src/main.rs
+
+src/main.rs:
---
+++ a_variables/target/debug/deps/a_variables-e45928e51b816b7a.d
@@ -0,0 +1,5 @@
+D:\rust_101\a_variables\target\debug\deps\liba_variables-e45928e51b816b7a.rmeta: src/main.rs
+
+D:\rust_101\a_variables\target\debug\deps\a_variables-e45928e51b816b7a.d: src/main.rs
+
+src/main.rs:
